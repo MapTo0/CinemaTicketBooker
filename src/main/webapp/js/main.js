0daf0e49359f89ba086688d3c051e806d6faf356
Martin Hristov <martin.r.hristov@gmail.com>
2015-06-02 17:14:43 +0300

    Front-end Ajax powered login/register functionality added, TODO: validation

---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+    var $LoginButton = $("#login-button"),
+        $RegisterButton = $("#register-button"),
+        oUserData = null,
+        oRegisterData = null;
+
+    $LoginButton.click(function() {
+        oUserData = {
+            user: $("#login-email").val(),
+            password: $("#login-password").val()
+        };
+
+        $.ajax({
+            url: 'rest/user/login',
+            type: "POST",
+            contentType: "application/json;charset=UTF-8",
+            data: JSON.stringify(oUserData),
+            statusCode: {
+                401: function() {
+                    alert("Authentication failed");
+                },
+                200: function() {
+                    alert("Authentication is fine!");
+                }
+            }
+        });
+    });
+
+    $RegisterButton.click(function() {
+        oRegisterData = {
+            user: $("#register-email").val(),
+            firstName: $("#register-firstName").val(),
+            lastName: $("#register-lastName").val(),
+            password: $("#register-password").val()
+        };
+
+        $.ajax({
+                url: 'rest/user/register',
+                type: "POST",
+                contentType: "application/json;charset=UTF-8",
+                data: JSON.stringify(oRegisterData)
+            })
+            .success(function(data) {
+                alert("Party brat");
+            })
+            .fail(function(data) {
+                alert("Sorry brat");
+            })
+            .always(function() {
+                $("#register-form").submit();
+            });
+    });
+});
