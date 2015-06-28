c46a09c6a4541e6f71642500c7d4f262a93c18b3
Alex Kushev <alex.kushev.bg@gmail.com>
2015-06-28 19:07:39 +0300

    fix

--- src/main/java/cinema/dao/TicketDAO.java
+++ src/main/java/cinema/dao/TicketDAO.java
@@ -3,42 +3,44 @@ package cinema.dao;
 import java.util.Collection;
 
 import javax.ejb.Singleton;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
+import cinema.model.Projection;
 import cinema.model.Ticket;
 
 @Singleton
 public class TicketDAO {
-	
+
 	@PersistenceContext
 	private EntityManager em;
-	
-	public void addTicket(Ticket ticket){
+
+	@Inject
+	private ProjectionDAO projectionDAO;
+
+	public void addTicket(Ticket ticket) {
 		em.persist(ticket);
 	}
-	
-	public Collection<Ticket> getAllTickets(){
+
+	public Collection<Ticket> getAllTickets() {
 		return em.createNamedQuery("getAllTickets", Ticket.class)
 				.getResultList();
 	}
-	
+
 	public Ticket findById(long key) {
 		return em.find(Ticket.class, key);
 	}
-	
-	public Ticket findByMovieTitle(String movieTitle) {
+
+	public Collection<Ticket> findByMovieTitle(String movieTitle) {
+		Projection projection = projectionDAO.findByMovieTitle(movieTitle);
 		TypedQuery<Ticket> query = em.createNamedQuery(
 				"findTicketByMovieTitle", Ticket.class).setParameter(
-				"movieTitle", movieTitle);
-		try {
-			return query.getSingleResult();
-		} catch (NoResultException e) {
-			return null;
-		}
-	}
-	
-}
+				"projection", movieTitle);
 
+		return null;
+	}
+
+}
--- src/main/java/cinema/services/CashierViewAllTickets.java
+++ src/main/java/cinema/services/CashierViewAllTickets.java
@@ -8,13 +8,14 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 import cinema.dao.UserDAO;
 import cinema.model.Ticket;
 import cinema.model.User;
 
 @Singleton
-@Path("/viewtickets")
+@Path("viewtickets")
 public class CashierViewAllTickets {
 
 	
@@ -22,13 +23,19 @@ public class CashierViewAllTickets {
 	private UserDAO userDAO;
 	
 	@GET
-	@Path("{email}")
+	@Path("/user")
 	@Produces("application/json")
-	public Collection<Ticket> getBoughtTickets(@PathParam("email") String email) {
+	public Collection<Ticket> getBoughtTicketsByUser(@QueryParam("email") String email) {
 		User user = userDAO.findUserByName(email);
 		if(user != null){
 			return user.getCurrentTickets();
 		}
+		return null;
+	}
+	
+	@GET
+	@Path("/projection")
+	public Collection<Ticket> getBoughtTicketsForProjection(@QueryParam("movieTitle") String movieTitle){
 		return null;
 	}
 }
